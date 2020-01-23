98a486673234e699040487f194c1acc6bd053881
José Alejandro Sabido Tamayo <alejandro_7351@hotmail.com>
2020-01-23 17:17:22 -0600

    singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using Singleton.Servicio;
+using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Unico s1 = Unico.ObtenerInstancia();
+            Unico s2 = Unico.ObtenerInstancia();
+
+            if(s1==s2)
+                Console.WriteLine("Patron singleton funciono");
+            else
+                Console.WriteLine("Patron singleton fallo");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton/Servicio/Unico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Singleton.Servicio
+{
+    public class Unico
+    {
+        private Unico() { }
+
+        private static Unico _instancia;
+
+        public static Unico ObtenerInstancia()
+        {
+            if(_instancia==null)
+                _instancia = new Unico();
+            return _instancia;
+        }
+
+        public static void RealizarLogica()
+        {
+
+        }
+    }
+}
